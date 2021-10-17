c6a2f15d60455333315878e84bed03a748f82d94
Nikita <iniktus@.mail.ru>
2021-10-17 11:35:15 +0700

    изменено:      task1_2_1/src/main/java/ru/nsu/nikita/MyStack.java 	изменено:      task1_2_1/src/main/java/ru/nsu/nikita/StackIterator.java 	изменено:      task1_2_1/src/test/java/ru/nsu/nikita/MyStackTests.java

--- task1_2_1/src/main/java/ru/nsu/nikita/MyStack.java
+++ task1_2_1/src/main/java/ru/nsu/nikita/MyStack.java
@@ -3,18 +3,18 @@ package ru.nsu.nikita;
 import java.util.Arrays;
 import java.util.EmptyStackException;
 
-public class MyStack<T> implements Iterable <T> {
+public class MyStack<T> implements Iterable<T> {
     private T[] stack;     //array of stack
     private int top;            //index of current element on the top of the heap
     private int size;           //current size of the array
 
     @SuppressWarnings("unchecked")
-    public MyStack () {     //MyStack constructor
+    public MyStack() {     //MyStack constructor
         this.size = 1;          //is 0 for correct element pushing and array resize
         this.top = 0;
         this.stack = (T[]) new Object[size];
     }
-    
+
     public void push(T newElem) {
         stack[top++] = newElem;     //add new element
         increaseSize();             //increase array size by 1
@@ -31,23 +31,26 @@ public class MyStack<T> implements Iterable <T> {
             top--;
             decreaseSize();
             return stack[top];
+        } else {
+            throw new EmptyStackException();
         }
-        else {throw new EmptyStackException();}
     }
-    
+
     public MyStack<T> popStack(int amount) throws EmptyStackException {
-        if (amount > this.top) {throw new EmptyStackException();} //throw exception, if user pops more elements than left in stack
+        if (amount > this.top) {
+            throw new EmptyStackException();
+        } //throw exception, if user pops more elements than left in stack
         MyStack<T> res = new MyStack<>();      //create result stack, to which this stack elements will be added
         for (int i = 0; i < amount; i++) {          //and return resulting stack
             res.push(this.pop());
         }
         return res;
     }
-    
+
     public int count() {
         return top;
     }
-    
+
     private void increaseSize() {       //increase array size in two times
         if (top == size) {
             size *= 2;
@@ -62,7 +65,7 @@ public class MyStack<T> implements Iterable <T> {
         }
     }
 
-    public StackIterator<T> iterator () {      //create iterator for this stack
+    public StackIterator<T> iterator() {      //create iterator for this stack
         return new StackIterator<>(this);
     }
 
