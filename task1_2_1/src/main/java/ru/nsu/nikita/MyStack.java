206813ccb15146487c7f21fa9c088992655ac5f1
Nikita <iniktus@.mail.ru>
2021-10-17 11:32:18 +0700

    изменено:      task1_2_1/src/main/java/ru/nsu/nikita/MyStack.java 	изменено:      task1_2_1/src/main/java/ru/nsu/nikita/StackIterator.java 	изменено:      task1_2_1/src/test/java/ru/nsu/nikita/MyStackTests.java

--- task1_2_1/src/main/java/ru/nsu/nikita/MyStack.java
+++ task1_2_1/src/main/java/ru/nsu/nikita/MyStack.java
@@ -3,54 +3,43 @@ package ru.nsu.nikita;
 import java.util.Arrays;
 import java.util.EmptyStackException;
 
-public class MyStack <T> {
-    private Object[] stack;     //array of stack
+public class MyStack<T> implements Iterable <T> {
+    private T[] stack;     //array of stack
     private int top;            //index of current element on the top of the heap
     private int size;           //current size of the array
 
     @SuppressWarnings("unchecked")
-    public <T> MyStack () {     //MyStack constructor
-        this.size = 0;          //is 0 for correct element pushing and array resize
+    public MyStack () {     //MyStack constructor
+        this.size = 1;          //is 0 for correct element pushing and array resize
         this.top = 0;
         this.stack = (T[]) new Object[size];
     }
     
     public void push(T newElem) {
-        increaseSize();             //increase array size by 1
         stack[top++] = newElem;     //add new element
+        increaseSize();             //increase array size by 1
     }
 
-    public void pushStack(MyStack <T> inStack) throws EmptyStackException {
-        StackIterator <T> iter = new StackIterator<>(inStack);      //for-each loop for inStack
-        while (iter.hasNext()) {            //push from inStack to this stack while inStack has unchecked elements
-            this.push(iter.next());
+    public void pushStack(MyStack<T> inStack) throws EmptyStackException {
+        for (T element : inStack) {            //push from inStack to this stack while inStack has unchecked elements
+            this.push(element);
         }
     }
 
-    @SuppressWarnings("unchecked")
     public T pop() throws EmptyStackException {
-        T res;
-        if (size > 0) {             //pop element if stack array is not empty yet
+        if (top > 0) {             //pop element if stack array is not empty yet
             top--;
-            res = (T) stack[top];
-            decreaseSize();         //and decrease array size by 1
+            decreaseSize();
+            return stack[top];
         }
-        else {              //if it is empty, throw exception
-            throw new EmptyStackException();
-        }
-
-        return res;
+        else {throw new EmptyStackException();}
     }
     
-    public MyStack <T> popStack(int am) {
-        MyStack <T> res = new MyStack<>();      //create result stack, to which this stack elements will be added
-        for (int i = 0; i < am; i++) {          //and return resulting stack
-            try {
-                res.push(this.pop());
-            }
-            catch (EmptyStackException ex) {    //if exception was caught during popping - return current stack
-                return res;
-            }
+    public MyStack<T> popStack(int amount) throws EmptyStackException {
+        if (amount > this.top) {throw new EmptyStackException();} //throw exception, if user pops more elements than left in stack
+        MyStack<T> res = new MyStack<>();      //create result stack, to which this stack elements will be added
+        for (int i = 0; i < amount; i++) {          //and return resulting stack
+            res.push(this.pop());
         }
         return res;
     }
@@ -59,24 +48,28 @@ public class MyStack <T> {
         return top;
     }
     
-    private void increaseSize() {       //increase array size by 1
+    private void increaseSize() {       //increase array size in two times
         if (top == size) {
-            stack = Arrays.copyOf(stack, size + 1);
-            size++;
+            size *= 2;
+            stack = Arrays.copyOf(stack, size);
         }
     }
 
-    private void decreaseSize() {       //decrease array size by 1
-        stack = Arrays.copyOf(stack, size-1);
-        size--;
+    private void decreaseSize() {      //decrease size in two times, if second half of array is empty. For better memory usage.
+        if (top < size / 2) {
+            size = top + 1;
+            stack = Arrays.copyOf(stack, size);
+        }
     }
 
-    public StackIterator <T> iterator () {      //create iterator for this stack
+    public StackIterator<T> iterator () {      //create iterator for this stack
         return new StackIterator<>(this);
     }
 
     @SuppressWarnings("unchecked")
     public T[] getStack() {             //return stack array
-        return (T[]) stack;
+        Object[] res = new Object[top];
+        System.arraycopy(stack, 0, res, 0, top);
+        return (T[]) res;
     }
 }
