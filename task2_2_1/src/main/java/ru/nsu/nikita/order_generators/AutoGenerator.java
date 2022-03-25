eb29fa1fe5ba806f1062644c0ad209e00cd5d5d4
Nikita <iniktus@.mail.ru>
2022-03-25 13:37:32 +0700

    Промежуточная версия.

--- task2_2_1/src/main/java/ru/nsu/nikita/order_generators/AutoGenerator.java
+++ task2_2_1/src/main/java/ru/nsu/nikita/order_generators/AutoGenerator.java
@@ -1,4 +1,20 @@
 package ru.nsu.nikita.order_generators;
 
+import ru.nsu.nikita.Pizzeria;
+
 public class AutoGenerator {
+
+    private final Pizzeria pizzeria;
+
+    public AutoGenerator(Pizzeria pizzeria) {
+        this.pizzeria = pizzeria;
+    }
+
+    public void generate(int ordersAmount) {
+        synchronized (pizzeria.getOrdersQueue()) {
+            for (int i = 0; i < ordersAmount; i++) {
+                pizzeria.getOrdersQueue().addLast(new Order(pizzeria.orderCounter.getAndAdd(1)));
+            }
+        }
+    }
 }
